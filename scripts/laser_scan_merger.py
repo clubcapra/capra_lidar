b78c9e45ca7a898a1901bf1bcd3ba69e0eca5b4c
MLefebvre <mathieuglefebvre@gmail.com>
2014-06-04 19:59:32 -0400

    Deplacement de launch file et début du laser scan matcher

--- scripts/laser_scan_merger.py
+++ scripts/laser_scan_merger.py
@@ -5,18 +5,29 @@ import rospy
 import csv
 from sensor_msgs.msg import LaserScan
 import time
+import math
 
 rf_data = None
 sg_data = None
 new_data = False
 
+def deg2rad(deg):
+    return deg * math.pi / 180
+
+angle_min = deg2rad(-135)
+angle_max = deg2rad(135)
+angle_increment = deg2rad(0.5) 
+last_scan_time = None
+
 def rf_callback(data):
     global rf_data
+    global new_data
     rf_data = data
     new_data = True
     
-def sg_callback(data);
+def sg_callback(data):
     global sg_data
+    global new_data
     sg_data = data
     new_data = True
 
@@ -31,10 +42,37 @@ def get_most_recent_timestamp(rf, sg):
     return stamp
 
 def merge_scans(rf, sg):
-    scan = LaserScan()
-    scan.header.frame_id = 'laser'
-    scan.header.stamp = get_most_recent_timestamp(rf, sg)    
-
+    if not sg:
+        rf.header.frame_id = 'laser'
+        return rf
+    else:
+        global angle_min
+        global angle_max
+        global angle_increment
+        global last_scan_time
+        if not last_scan_time:
+            last_scan_time = time.time()
+        
+        scan = LaserScan()
+        scan.header.frame_id = 'laser'
+        scan.header.stamp = get_most_recent_timestamp(rf, sg)    
+        scan.angle_min = angle_min
+        scan.angle_max = angle_max
+        scan.angle_increment = angle_increment
+        scan.scan_time = time.time() - last_scan_time
+        scan.time_increment = scan.scan_time / 541
+        scan.range_min = rf.range_min
+        scan.range_max = rf.range_max
+        if not sg:
+            scan.ranges = rf.ranges
+        else:
+            for i in range(len(rf.ranges)):
+                rrf = rf.ranges[i]
+                rsg = sg.ranges[i]
+                if rrf < rsg:
+                    scan.ranges.append(rrf)
+                else:
+                    scan.ranges.append(rsg)
 
     return scan
 
@@ -51,3 +89,4 @@ while not rospy.is_shutdown():
     time.sleep(0.001)
 
 rospy.spin()
+

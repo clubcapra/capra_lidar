3f83c43ec9ff90866b195fc78709b4e487ce175e
Mathieu Gascon-Lefebvre <mathieuglefebvre@gmail.com>
2014-07-15 17:46:11 -0400

    Modification de fake_imu pour toujours publier un angle, même s'il ne recoit rien

--- scripts/fake_imu.py
+++ scripts/fake_imu.py
@@ -1,31 +1,36 @@
 #! /usr/bin/env python
-
-import roslib; roslib.load_manifest('capra_lidar')
+import roslib
+roslib.load_manifest('capra_lidar')
 import rospy
-import csv
+from threading import Event
 from sensor_msgs.msg import Imu
-from geometry_msgs.msg import Pose2D, Quaternion
-import time
-import math
+from geometry_msgs.msg import Pose2D
 from tf.transformations import quaternion_from_euler
 
-def callback(data):
-    imu = Imu()
-    quat = quaternion_from_euler(0, 0, data.theta)
-    imu.orientation.z = quat[2]
-    imu.orientation.w = quat[3]
-    imu.orientation_covariance = [0.001225, 0,        0,
-                                  0,        0.001225, 0,
-                                  0,        0,        0.001225]
-                                  
-    imu.header.stamp = rospy.Time.now()
-    imu.header.frame_id = "/base_footprint"
-    
-    global pub
-    pub.publish(imu)
-    pass
 
+def callback(data):
+    global angle, evt
+    angle = data.theta
+    evt.set()
+
+
+evt = Event()
+angle = 0.0
 rospy.init_node('fake_imu', anonymous=True)
 pub = rospy.Publisher('/imu_data', Imu, queue_size=10)
 rospy.Subscriber("/pose2D", Pose2D, callback)
-rospy.spin()
+
+while not rospy.is_shutdown():
+    evt.wait(timeout=1.0/60)
+    evt.clear()
+    imu = Imu()
+    quat = quaternion_from_euler(0, 0, angle)
+    imu.orientation.z = quat[2]
+    imu.orientation.w = quat[3]
+    imu.orientation_covariance = [0, 0, 0,
+                                  0, 0, 0,
+                                  0, 0, 0.0001225]
+
+    imu.header.stamp = rospy.Time.now()
+    imu.header.frame_id = "/base_footprint"
+    pub.publish(imu)
